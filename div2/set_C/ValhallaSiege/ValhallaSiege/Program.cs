d92ede189249e2d690cb8af92a07d8d049c1e2ba
YI ZHANG <scaccterran@gmail.com>
2018-05-01 18:52:27 -0400

    init commit

---
+++ div2/set_C/ValhallaSiege/ValhallaSiege/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValhallaSiege {
+    class Program {
+        static void Main(string[] args) {
+
+            var siege = new ValhallaSiege();
+            Console.WriteLine(string.Join("\n", siege.CountDeath("1 2 1 2 1", "3 10 1 1 1")));
+            Console.WriteLine(string.Join("\n", siege.CountDeath("1 2 3 4", "9 1 10 6")));
+        }
+    }
+}
